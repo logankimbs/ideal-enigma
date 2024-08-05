76025b6dba3f7420aeea6e2fdb68d4d5e52c48fb
logankimbs <logankimbs@gmail.com>
2024-08-05 15:55:58 -0700

    nice

--- src/database/entities/BaseEntity.ts
+++ src/database/entities/BaseEntity.ts
@@ -9,6 +9,7 @@ export abstract class BaseEntity {
   @PrimaryGeneratedColumn("uuid")
   id!: string;
 
+<<<<<<< HEAD
   @CreateDateColumn({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
   createdAt!: Date;
 
@@ -17,6 +18,12 @@ export abstract class BaseEntity {
     nullable: true,
     onUpdate: "CURRENT_TIMESTAMP",
   })
+=======
+  @CreateDateColumn({ type: "timestamptz" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: "timestamptz" })
+>>>>>>> d1bc996 (nice)
   updatedAt!: Date;
 
   @DeleteDateColumn({ type: "timestamptz", nullable: true })
