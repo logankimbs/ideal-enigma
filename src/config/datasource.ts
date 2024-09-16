499943920e81fd77039656191586115a5f7de334
logankimbs <logankimbs@gmail.com>
2024-09-16 13:13:47 -0700

    testing

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -9,6 +9,9 @@ import logger from "../utils/logger";
 
 const baseDatasource: DataSourceOptions = {
   type: "postgres",
+
+
+  
   host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USERNAME,
