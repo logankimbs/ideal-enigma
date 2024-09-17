23d705bba03eda5718b90a50a9c8e3766776e644
logankimbs <logankimbs@gmail.com>
2024-09-17 11:14:17 -0700

    Update function name

--- src/listeners/events/index.ts
+++ src/listeners/events/index.ts
@@ -1,12 +1,12 @@
 import { App } from "@slack/bolt";
 import appHomeOpened from "./appHomeOpened";
 import teamJoin from "./teamJoin";
-import userChange from "./userProfileChanged";
+import userProfileChanged from "./userProfileChanged";
 
 const register = (app: App) => {
   app.event("app_home_opened", appHomeOpened);
   app.event("team_join", teamJoin);
-  app.event("user_profile_changed", userChange);
+  app.event("user_profile_changed", userProfileChanged);
 };
 
 export default { register };
