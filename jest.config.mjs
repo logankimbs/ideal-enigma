2b3c5bcb2cf6e00eeac8e70708b6d10a9fa2b616
logankimbs <logankimbs@gmail.com>
2024-09-17 09:32:59 -0700

    Updated tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,6 +2,6 @@ export default {
   preset: "ts-jest",
   roots: ["<rootDir>"],
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testMatch: ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
   transform: { "^.+\\.ts$": "ts-jest" },
 };
