dc9910e2e11075dd98529eb314de6d6d7395099e
logankimbs <logankimbs@gmail.com>
2024-09-17 12:52:43 -0700

    Add team join event

--- src/listeners/events/teamJoin.ts
+++ src/listeners/events/teamJoin.ts
@@ -1,17 +1,28 @@
 import { AllMiddlewareArgs, SlackEventMiddlewareArgs } from "@slack/bolt";
 import logger from "../../utils/logger";
+import { teamRepo, userRepo } from "../../repositories";
+import { UserEntity } from "../../entities";
+import { UserData } from "../../types";
 
 const teamJoin = async ({
-  client,
   event,
 }: AllMiddlewareArgs & SlackEventMiddlewareArgs<"team_join">) => {
+  logger.info(`Adding new user to team ${event.user.team_id}`);
+
   try {
-    console.log("user added ");
-    logger.info("client", client);
-    logger.info("event", event);
-    logger.info("user", event.user);
+    const data = event.user;
+    const team = await teamRepo.findOneByOrFail({ id: data.team_id });
+    const user = {
+      id: data.id,
+      team,
+      data: data as UserData,
+    } as UserEntity;
+
+    userRepo.save(user);
+
+    logger.info(`Saved new user ${data.id}`);
   } catch (error) {
-    console.error("Error saving new user:", error);
+    logger.error("Error saving new user:", error);
   }
 };
 
