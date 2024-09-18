66c386a2bc470f1c889c34070ae2932c633c4e1b
logankimbs <logankimbs@gmail.com>
2024-09-18 08:55:21 -0700

    Removed blocks dir and placed blocks in correct folders

--- src/listeners/actions/open-modal.ts
+++ src/listeners/actions/open-modal.ts
@@ -3,10 +3,25 @@ import {
   BlockAction,
   SlackActionMiddlewareArgs,
 } from "@slack/bolt";
-import { getModalBlocks } from "../../blocks/modal-blocks";
 import { HISTORY_VIEW_CALLBACK_ID } from "../views/history-view";
+import {
+  createContextBlock,
+  createInputBlock,
+  createSectionBlock,
+} from "../../utils/blocks";
 
 export const OPEN_MODAL = "open_modal";
+
+const getModalBlocks = () => [
+  createSectionBlock(
+    "plain_text",
+    "We hope you accomplished a lot this week! More importantly, we hope you learned a lot. Submitting and sharing these learnings with your team will empower the entire organization to move faster and smarter.",
+  ),
+  createInputBlock("What was the most impactful thing you learned this week?"),
+  createContextBlock(
+    "For guidance on what makes a learning impactful, <https://google.com|click here>.",
+  ),
+];
 
 const openModal = async ({
   ack,
