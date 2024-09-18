28ff75ebd02af904144ed1324042d7b9f4519044
logankimbs <logankimbs@gmail.com>
2024-09-18 09:38:12 -0700

    Updated InstallationEntity

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,4 +1,5 @@
 export { default as InsightEntity } from "./InsightEntity";
+export { default as InstallationEntity } from "./InstallationEntity";
 
 export * from "./InstallationEntity";
 export * from "./TeamEntity";
