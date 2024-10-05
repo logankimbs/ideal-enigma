7edd6de5def9c0764a626c00b734e7d273b50c6a
logankimbs <logankimbs@gmail.com>
2024-10-05 10:06:43 -0700

    Added shared types to api

---
+++ apps/api/src/types/index.ts
@@ -0,0 +1,5 @@
+export type { Insight } from "../modules/insight";
+export type { Installation } from "../modules/installation";
+export type { Tag } from "../modules/tag";
+export type { Team } from "../modules/team";
+export type { User } from "../modules/user";
