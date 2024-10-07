bd73bb3d643b5ae5542aef4bde0daa98b6dd7432
logankimbs <logankimbs@gmail.com>
2024-10-07 16:52:34 -0700

    Cleaned build process

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,10 +4,13 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    // TODO: Change for production
+    origin: "http://localhost:3000",
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3001);
+  await app.listen(4000);
 }
 
 bootstrap();
