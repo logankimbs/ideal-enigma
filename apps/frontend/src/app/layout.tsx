d09681516c3d37071d94c28a6e2671c0add0f0d7
logankimbs <logankimbs@gmail.com>
2024-11-03 08:19:14 -0800

    Add picture

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -16,17 +16,6 @@ type RootLayoutProps = {
 
 export default async function RootLayout(props: RootLayoutProps) {
   return (
-    // <html
-    //   lang="en"
-    //   className="text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
-    // >
-    //   <head>
-    //     <link rel="preconnect" href="https://rsms.me/" />
-    //     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    //   </head>
-    //   <body>{props.children}</body>
-    // </html>
-
     <html lang="en">
       <head>
         <link
@@ -40,7 +29,7 @@ export default async function RootLayout(props: RootLayoutProps) {
           href="/blog/feed.xml"
         />
       </head>
-      <body className="text-gray-950 antialiased">{props.children}</body>
+      <body className="dark:text-white antialiased">{props.children}</body>
     </html>
   );
 }
