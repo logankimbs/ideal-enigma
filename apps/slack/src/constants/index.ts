831ee17f1d47f8eba1b46791bd5e469fb998c365
logankimbs <logankimbs@gmail.com>
2024-11-08 16:28:19 -0800

    Updated home tab verbage in slack

--- apps/slack/src/constants/index.ts
+++ apps/slack/src/constants/index.ts
@@ -3,16 +3,15 @@ export const SUBMIT_INSIGHT = "submit_insight";
 export const AUTO_LOGIN = "auto_login";
 
 export const INSIGHT_MODAL_TEXTS = {
-  TITLE: "Submit an Insight!",
+  TITLE: 'Submit an Insight',
   DESCRIPTION:
-    "Your insights are invaluable. Share what you’ve learned to empower your team. Submit an insight and let’s achieve greatness together!",
+    'Your insights are invaluable. Share what you’ve learned to empower your team. Submit an insight and let’s achieve greatness together!',
   PLACEHOLDER:
-    "Try this format: I learned [x] by [doing y], so I suggest we [z], and therefore [e].",
-  TAGS_PLACEHOLDER: "e.g., marketing, launch, feedback",
-  LINKS_PLACEHOLDER: "e.g., https://google.com",
-  HELP_TEXT:
-    "💡*Need help?* For tips on crafting impactful insights, <https://google.com|click here>.",
+    'Try this format: I learned [x] by [doing y], so I suggest we [z], and therefore [e].',
+  TAGS_PLACEHOLDER: 'e.g., marketing, launch, feedback',
+  LINKS_PLACEHOLDER: 'e.g., https://google.com',
+  HELP_TEXT: `💡 Need tips for impactful insights? <${process.env.FRONTEND_URL}|Click here>.`,
   PRO_TIP:
-    "🔥 *Pro Tip:* You can add multiple tags by separating them with commas.",
-  SUBMIT_BUTTON: "Submit Insight",
+    '🔥 *Pro Tip:* You can add multiple tags by separating them with commas.',
+  SUBMIT_BUTTON: 'Submit',
 };
--- apps/slack/src/listeners/events/appHomeOpened.ts
+++ apps/slack/src/listeners/events/appHomeOpened.ts
@@ -15,48 +15,48 @@ const getHomeViewBlocks = (event: any) => {
   slackAuthUrl.search = new URLSearchParams({ ...params }).toString();
 
   return [
-    createHeaderBlock("👋 Meet Echo"),
+    createHeaderBlock('👋 Meet Echo'),
     createSectionWithButton({
       text: "I'm here to help you recall and submit your weekly insights and share them with your teammates in order to circulate and accelerate the rate at which your team can build, measure, and learn. By frequently sharing your insights, you are contributing to building a culture of continuous learning.",
-      buttonText: "Learn More",
-      buttonValue: "learn_more",
+      buttonText: '🔍 Learn More',
+      buttonValue: AUTO_LOGIN,
+      url: `${process.env.FRONTEND_URL}`,
     }),
-    { type: "divider" },
     createSectionWithButton({
-      text: "*What makes an insight impactful?*\n\nAn impactful insight drives meaningful action, leading to improved performance, user satisfaction, and business growth.",
-      buttonText: "Impactful Insights",
-      buttonValue: "try_journeys",
+      text: '*What makes an insight impactful?*\nAn impactful insight sparks action and fuels growth. Here’s what makes it effective:',
+      buttonText: '🚀 Impactful Insights',
+      buttonValue: AUTO_LOGIN,
+      url: `${process.env.FRONTEND_URL}`,
     }),
     createRichTextBlock(
-      "1. Clear, Actionable, and Specific:",
-      " Insights should come with clear, actionable recommendations that teams can act on. Specific, detailed insights that pinpoint exact areas for improvement are more useful.",
+      '1. Clear, Actionable, and Specific:',
+      ' Insights should include specific recommendations that teams can act on right away.'
     ),
     createRichTextBlock(
-      "2. Data-Driven:",
-      " Insights should be grounded in concrete qualitative or quantitative data, ensuring they are reliable and credible.",
+      '2. Data-Driven:',
+      ' Ground insights in reliable data for credibility.'
     ),
     createRichTextBlock(
-      "3. Relevant and Timely:",
-      " Insights should be relevant to the company’s strategic objectives, helping to drive key metrics like user retention, revenue growth, or operational efficiency.",
+      '3. Relevant and Timely:',
+      ' Align insights with strategic goals to drive key metrics.'
     ),
     createRichTextBlock(
-      "4. Clearly Communicated:",
-      " Insights should be communicated clearly, avoiding jargon and ensuring they are understandable to all stakeholders.",
+      '4. Simple Communication:',
+      ' Avoid jargon and keep insights understandable for anyone.'
     ),
-    { type: "divider" },
     createRichTextBlock(
-      "Here is a good example of an impactful insight:",
-      "Analyzing user behavior has revealed that simplifying the onboarding process by reducing the steps from 8 to 5 increases user activation rates by 30%.",
+      'Here is a good example of an impactful insight:',
+      ' Analyzing user behavior has revealed that simplifying the onboarding process by reducing the steps from 8 to 5 increases user activation rates by 30%.'
     ),
-    createHeaderBlock("Do More With Echo"),
+    createHeaderBlock('Do More with Echo'),
     createSectionWithButton({
-      text: "📄 *Submit a Quick Insight*\nCreate an Intros, Watercooler, or Celebrations channel.",
-      buttonText: "Submit an Insight",
+      text: '*Submit a Quick Insight*\nSubmit your learnings to share with the team.',
+      buttonText: '📩 Submit an Insight',
       buttonValue: OPEN_INSIGHT_MODAL,
     }),
     createSectionWithButton({
-      text: "*Checkout your Dashboard*",
-      buttonText: "Go to Dashboard",
+      text: '*Explore the Echo Dashboard*\nAccess Echo settings, reporting, and the growing repository of insights.',
+      buttonText: '📈 Visit Echo Dashboard',
       url: `${slackAuthUrl}`,
       buttonValue: AUTO_LOGIN,
     }),
