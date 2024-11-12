d2ee28a8263e591d584455cde942c19cd852cb9e
logankimbs <logankimbs@gmail.com>
2024-11-12 13:01:06 -0800

    Added synchronize to db config

--- apps/backend/src/config/config.module.ts
+++ apps/backend/src/config/config.module.ts
@@ -5,19 +5,19 @@ const config = () => {
   const isDev = process.env.NODE_ENV === "development";
 
   return {
-    port: parseInt(process.env.PORT || "4000", 10),
-    jwtSecret: process.env.JWT_SECRET || "jwt_secret",
+    port: parseInt(process.env.PORT || '4000', 10),
+    jwtSecret: process.env.JWT_SECRET || 'jwt_secret',
     origin: {
-      frontend: process.env.FRONTEND_URL || "http://localhost:3000",
-      slack: process.env.SLACK_URL || "http://localhost:8080",
+      frontend: process.env.FRONTEND_URL || 'http://localhost:3000',
+      slack: process.env.SLACK_URL || 'http://localhost:8080',
     },
     database: {
-      port: parseInt(process.env.DATABASE_PORT || "5432", 10),
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
       host: process.env.DATABASE_HOST,
       name: process.env.DATABASE_NAME,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
-      synchronize: isDev,
+      synchronize: false, // FIXME: THIS IS VERY BAD!
       ssl: !isDev ? { rejectUnauthorized: false } : false,
     },
     slack: {
