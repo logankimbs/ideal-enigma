2ec9f2ceec5bb2b3bdcbe17f35dd17b9a31b6ae8
logankimbs <logankimbs@gmail.com>
2024-11-12 12:55:45 -0800

    Updated next.config to run in prod

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,11 +1,23 @@
-// @ts-check
-const { composePlugins, withNx } = require('@nx/next');
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+const dotenv = require('dotenv');
+const path = require('path');
 
-/** @type {import('@nx/next/plugins/with-nx').WithNxOptions} **/
-const nextConfig = {
-  nx: { svgr: false },
+// Load environment variables from the root .env file
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+/** @type {import('next').NextConfig} */
+module.exports = (phase, { defaultConfig }) => {
+  const nextConfig = {};
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    // Only load @nx/next in development
+    const { composePlugins, withNx } = require('@nx/next');
+    const plugins = [withNx];
+
+    nextConfig.nx = { svgr: false };
+
+    return composePlugins(...plugins)(nextConfig);
+  }
+
+  return nextConfig;
 };
-
-const plugins = [withNx];
-
-module.exports = composePlugins(...plugins)(nextConfig);
