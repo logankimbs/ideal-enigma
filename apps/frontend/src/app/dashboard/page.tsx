d8134802eabf43a85dc38de690c52f7479106806
Logan Kimball <logankimbs@gmail.com>
2024-12-05 12:10:58 -0800

    Cleaned dashboard home page

--- apps/frontend/src/app/dashboard/page.tsx
+++ apps/frontend/src/app/dashboard/page.tsx
@@ -13,44 +13,24 @@ export default async function Home() {
       <Heading>Good afternoon!</Heading>
       <div className="mt-8 flex items-end justify-between">
         <Subheading>My Weekly Overview</Subheading>
-        {/*<div>*/}
-        {/*  <Select name="period">*/}
-        {/*    <option value="last_week">Last week</option>*/}
-        {/*    <option value="last_two">Last two weeks</option>*/}
-        {/*    <option value="last_month">Last month</option>*/}
-        {/*    <option value="last_quarter">Last quarter</option>*/}
-        {/*  </Select>*/}
-        {/*</div>*/}
       </div>
       <div className="mt-4 grid gap-8 sm:grid-cols-2 xl:grid-cols-4">
-        <Stat title="Total insights" value="$2.6M" change="+4.5%" />
-        <Stat title="Total themes" value="$455" change="-0.5%" />
-        <Stat title="Average insights" value="5,888" change="+4.5%" />
-        {/* constant */}
-        {/* 0-1 no flame*/}
-        {/* 1-3 1 flame*/}
-        {/* 3-5 2 flame*/}
-        {/* 5+ 3 flame*/}
+        <Stat title="Total insights" value="87" change="+4.5%" />
+        <Stat title="Total themes" value="10" change="-0.5%" />
+        <Stat title="Average insights" value="0.8" change="+4.5%" />
+        {/* 0-1 no flame. 1-3 one flame. 3-5 two flames. 5+ three flames. */}
         <Stat title="Current streak" value="4" change="🔥🔥🔥" />
       </div>
       <div className="mt-8 flex items-end justify-between">
         <Subheading>Company Weekly Overview</Subheading>
-        {/*<div>*/}
-        {/*  <Select name="period">*/}
-        {/*    <option value="last_week">Last week</option>*/}
-        {/*    <option value="last_two">Last two weeks</option>*/}
-        {/*    <option value="last_month">Last month</option>*/}
-        {/*    <option value="last_quarter">Last quarter</option>*/}
-        {/*  </Select>*/}
-        {/*</div>*/}
       </div>
       <div className="mt-4 grid gap-8 sm:grid-cols-2 xl:grid-cols-4">
-        <Stat title="Total insights" value="$2.6M" change="+4.5%" />
-        <Stat title="Total themes" value="$455" change="-0.5%" />
-        <Stat title="Average insights per user" value="5,888" change="+4.5%" />
-        <Stat title="Active contributors" value="823,067" change="+21.2%" />
+        <Stat title="Total insights" value="2,746" change="+4.5%" />
+        <Stat title="Total themes" value="150" change="-0.5%" />
+        <Stat title="Average insights per user" value="2" change="+4.5%" />
+        <Stat title="Active contributors" value="150" change="+21.2%" />
       </div>
-      <div className="mt-8 flex grid gap-8 sm:grid-cols-1 xl:grid-cols-2">
+      <div className="mt-8 grid gap-8 sm:grid-cols-1 xl:grid-cols-2">
         {recentSummary && <SummaryPreview summary={recentSummary} />}
         {recentInsights && recentInsights.length > 0 && (
           <RepositoryPreview repository={recentInsights} />
