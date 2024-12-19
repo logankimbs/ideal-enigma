b52c913172286445750a11cbb24ed87fa2e2f98c
Logan Kimball <logankimbs@gmail.com>
2024-12-19 08:41:51 -0800

    Added slack button to footer

--- apps/frontend/src/components/slack-install-button.tsx
+++ apps/frontend/src/components/slack-install-button.tsx
@@ -3,15 +3,19 @@ import { Button } from './landing/button';
 
 type Props = {
   logo?: boolean;
+  className?: string;
 };
 
-export function SlackInstallButton({ logo = true }: Props) {
-  console.log('Backend URL:', process.env.NEXT_PUBLIC_BACKEND_URL);
-
+export function SlackInstallButton({
+  logo = true,
+  className,
+  ...props
+}: Props) {
   return (
     <Button
       href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/slack/install`}
-      className={clsx({ 'gap-2': logo })}
+      className={clsx({ 'gap-2': logo }, className)}
+      {...props}
     >
       {logo && (
         // eslint-disable-next-line @next/next/no-img-element
