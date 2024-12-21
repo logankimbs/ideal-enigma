0303a42ddbaf6f6d511e3a096bd44d568364f222
Logan Kimball <logankimbs@gmail.com>
2024-12-21 10:52:00 -0800

    Added gradient background

--- apps/frontend/src/components/landing/gradient.tsx
+++ apps/frontend/src/components/landing/gradient.tsx
@@ -20,7 +20,7 @@ export function GradientBackground() {
     <div className={'relative mx-auto max-w-7xl'}>
       <div
         className={clsx(
-          'absolute -right-60 -top-44 h-60 w-[36rem] transform-gpu md:right-0',
+          '-z-50 absolute -right-60 -top-44 h-60 w-[36rem] transform-gpu md:right-0',
           'bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff]',
           'rotate-[-10deg] rounded-full blur-3xl'
         )}
--- apps/frontend/src/components/sidebar-layout.tsx
+++ apps/frontend/src/components/sidebar-layout.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
-import * as Headless from "@headlessui/react";
-import React, { useState } from "react";
-import { NavbarItem } from "./navbar";
+import * as Headless from '@headlessui/react';
+import React, { useState } from 'react';
+import { GradientBackground } from './landing/gradient';
+import { NavbarItem } from './navbar';
 
 function OpenMenuIcon() {
   return (
@@ -83,7 +84,10 @@ export function SidebarLayout({
 
       {/* Content */}
       <main className="flex flex-1 flex-col pb-2 lg:min-w-0 lg:pl-64 lg:pr-2 lg:pt-2">
-        <div className="grow p-6 lg:rounded-lg lg:bg-white lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
+        <div className="grow p-6 lg:rounded-lg lg:bg-white/50 lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900/50 dark:lg:ring-white/10">
+          <div className="hidden lg:block">
+            <GradientBackground />
+          </div>
           <div className="mx-auto max-w-6xl">{children}</div>
         </div>
       </main>
