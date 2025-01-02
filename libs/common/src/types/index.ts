3e3fa591753b98cfb2a0b62f485e308d16c12883
Logan Kimball <logankimbs@gmail.com>
2025-01-02 10:53:54 -0800

    Removed unused shared types

--- libs/common/src/types/index.ts
+++ libs/common/src/types/index.ts
@@ -1,32 +1,6 @@
-export type ActiveContributors = {
-  this_week_avg: number;
-  change_percent: number;
-};
-
-export type AverageInsights = {
-  average_including_current: number;
-  average_excluding_current: number;
-  change_from_excluding_to_including: number;
-};
-
-export type TotalInsights = {
-  last_7_days_count: string;
-  previous_7_days_count: string;
-  relative_difference_percent: string;
-};
-
 export type Stat = {
   value: string;
   change: string;
-};
-
-export type UserStreak = {
-  count: number;
-};
-
-export type Average = {
-  week_start: Date;
-  insight_count: string;
 };
 
 export type UserStats = {
