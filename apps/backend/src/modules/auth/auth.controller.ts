b03e9239dc6b2728b5676862d26d257c616c7115
Logan Kimball <logankimbs@gmail.com>
2025-01-11 10:31:43 -0800

    Cleaned up auth

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -1,57 +1,12 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Param,
-  Post,
-  Query,
-  Redirect,
-} from '@nestjs/common';
-import { Public } from '../../common/constants';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginDto } from './dto/login.dto';
-import { SlackAuthorizeDto } from './dto/slack-authorize.dto';
-import { SlackCallbackDto } from './dto/slack-callback.dto';
-import { ValidateTokenDto } from './dto/validate-token.dto';
-
-const frontend_url = process.env.FRONTEND_URL;
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Get('code/:userId')
-  async generateLoginCode(@Param('userId') userId: string) {
-    return await this.authService.generateLoginCode(userId);
-  }
-
-  @Public()
-  @HttpCode(HttpStatus.OK)
-  @Post('slack-login')
-  login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto);
-  }
-
-  @Public() // Should this be public?
-  @HttpCode(HttpStatus.OK)
-  @Post('validate-token')
-  validateToken(@Body() validateTokenDto: ValidateTokenDto) {
-    return this.authService.validateToken(validateTokenDto);
-  }
-
-  @Public()
-  @Get('slack')
-  @Redirect('https://slack.com', 302)
-  slackAuthorize(@Query() slackAuthorizeDto: SlackAuthorizeDto) {
-    return this.authService.slackAuthorize(slackAuthorizeDto);
-  }
-
-  @Public()
-  @Get('slack/callback')
-  @Redirect(frontend_url, 302)
-  async slackCallback(@Query() slackCallbackDto: SlackCallbackDto) {
-    return this.authService.slackCallback(slackCallbackDto);
+  async getAuthCode(@Param('userId') userId: string) {
+    return await this.authService.getAuthCode(userId);
   }
 }
