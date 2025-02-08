35ff4e312614cbeea800c0e6c8170eaaa731fb0a
Logan Kimball <logankimbs@gmail.com>
2025-02-08 07:45:20 -0800

    Added team module

--- apps/backend/src/modules/users/users.module.ts
+++ apps/backend/src/modules/users/users.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Team } from '../../infra/database/entities/team.entity';
 import { User } from '../../infra/database/entities/user.entity';
+import { TeamsModule } from '../teams/teams.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Team])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => TeamsModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
