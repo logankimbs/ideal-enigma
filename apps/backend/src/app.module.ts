695f8a877d96b6d5164b210f5fc7e2cdd9b3c0f6
Logan Kimball <logankimbs@gmail.com>
2025-02-09 07:14:17 -0800

    Added schedule reminder task to app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -14,10 +14,15 @@ import { InstallationsModule } from './modules/installations/installations.modul
 import { SummariesModule } from './modules/summaries/summaries.module';
 import { GenerateSummariesTaskModule } from './modules/summaries/tasks/generate-summaries.task.module';
 import { TagsModule } from './modules/tags/tags.module';
+import { ScheduleTeamReminderTaskModule } from './modules/teams/tasks/schedule-team-reminder.task.module';
 import { TeamsModule } from './modules/teams/teams.module';
 import { UsersModule } from './modules/users/users.module';
 
-const TaskModules = [GenerateSummariesTaskModule];
+const TaskModules = [
+  GenerateSummariesTaskModule,
+  ScheduleTeamReminderTaskModule,
+];
+
 const Modules = [
   AuthModule,
   InsightsModule,
