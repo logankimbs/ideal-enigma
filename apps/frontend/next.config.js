ce99129471cdcfca5b38e9b9ab094d8eab9a7c03
logankimbs <logankimbs@gmail.com>
2025-04-01 13:21:04 -0700

    Update next.config

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,17 +2,19 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 const dotenv = require('dotenv');
 const path = require('path');
 
-// Load environment variables from the root .env file
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 /** @type {import('next').NextConfig} */
 module.exports = (phase, { defaultConfig }) => {
   const nextConfig = {
     distDir: '../../dist/apps/frontend/.next',
+
+    env: {
+      NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    },
   };
 
   if (phase === PHASE_DEVELOPMENT_SERVER) {
-    // Only load @nx/next in development
     const { composePlugins, withNx } = require('@nx/next');
     const plugins = [withNx];
 
