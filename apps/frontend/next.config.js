91433b838105f06f902e498492372b90a0538d44
logankimbs <logankimbs@gmail.com>
2025-04-03 08:23:50 -0700

    Removed workflow

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,15 +2,14 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 const dotenv = require('dotenv');
 const path = require('path');
 
-// Load environment variables from the root .env file
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-
 /** @type {import('next').NextConfig} */
 module.exports = (phase, { defaultConfig }) => {
   const nextConfig = {};
 
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     // Only load @nx/next in development
+    dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+    
     const { composePlugins, withNx } = require('@nx/next');
     const plugins = [withNx];
 
